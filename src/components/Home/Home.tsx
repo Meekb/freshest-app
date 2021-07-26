c67e3610ec7cd5bcf5128aaeaf9af8c47adef951
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-07-26 14:40:12 -0600

    Add skeleton of Home component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,3 +1,54 @@
-import React from "react";
+import React, { useState } from 'react'
 
-export {}
+interface ApiMarkets {
+  markets: {
+    id: number,
+    marketName: string
+    //need logic to split the distance from the name to display the name
+  }[],
+  marketDetails: {
+    googlink: string,
+    address: string,
+    schedule: string,
+    products: string
+    // need logic for splitting products string into an array
+  }[]
+}
+
+// interface ApiDetails {
+//   marketDetails: {
+//     googlink: string,
+//     address: string,
+//     schedule: string,
+//     products: string
+//     // need logic for splitting products string into an array
+//   }
+// }
+
+// const Home = ():JSX.Element => {
+//   const [ allMarkets, marketDetails ] = useState<ApiMarkets>()
+
+//   return (
+//     <div className="home">
+
+//     </div>
+//   )
+// }
+
+class Home extends React.Component<ApiMarkets> {
+  state: ApiMarkets = {
+    markets: [],
+    marketDetails: []
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+
+export default Home;
