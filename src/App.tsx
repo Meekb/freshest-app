d7a6312c0ea539c04ca36bbd9f4c7ffe53e5da07
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-07-26 15:55:04 -0600

    Add code for eventual fetch call in Home, prep for future components in render

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Home from  "./components/Home/Home"
 interface AppProps {}
 
 export const App: React.FC<AppProps> = ({}) => {
   return (
     <main>
       <h1>Hi</h1>
+      <Home />
     </main>
   );
 };
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 interface ApiMarkets {
   markets: {
@@ -15,40 +15,65 @@ interface ApiMarkets {
   }[]
 }
 
-// interface ApiDetails {
-//   marketDetails: {
-//     googlink: string,
-//     address: string,
-//     schedule: string,
-//     products: string
-//     // need logic for splitting products string into an array
+// Written with hooks below:
+const Home = ():JSX.Element => {
+  const [ allMarkets, setMarkets ] = useState<ApiMarkets["markets"]>([])
+  const [ marketDetails, setDetails ] = useState<ApiMarkets["marketDetails"]>([])
+  const [ error, setError ] = useState('')
+  
+  const getMarkets = async () => {
+    const url = ''
+    setError('')
+
+    try {
+      const response = await fetch(url)
+      const markets = await response.json()
+      setMarkets(markets)
+    } catch(error) {
+      setError(error.message)
+    }
+  }
+
+  useEffect(() => {
+    getMarkets()
+  }, [])
+  
+  return (
+    <div className="home">
+      <h2>yellow</h2>
+      {/* <Search allMarkets={allMarkets} marketDetails={marketDetails}/> */}
+      {/* <Results allMarkets={allMarkets} marketDetails={marketDetails}/> */}
+    </div>
+  )
+}
+
+
+//written as a class component without hooks below
+// class Home extends React.Component<ApiMarkets>{
+//   state = {
+//     markets: [
+//         {
+//           id: 99,
+//           marketName: "Friends"}
+//         ],
+//     marketDetails: [
+//       {
+//         googlink: "string",
+//         address: "234 marion",
+//         schedule: "tuesday",
+//         products: "egg, brownies"
+//       }
+//     ]
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>yellow</h1>
+//       </div>
+//     )
 //   }
 // }
-
-// const Home = ():JSX.Element => {
-//   const [ allMarkets, marketDetails ] = useState<ApiMarkets>()
-
-//   return (
-//     <div className="home">
-
-//     </div>
-//   )
-// }
-
-class Home extends React.Component<ApiMarkets> {
-  state: ApiMarkets = {
-    markets: [],
-    marketDetails: []
-  }
-
-  render() {
-    return (
-      <div>
-
-      </div>
-    )
-  }
-}
 
 
 export default Home;
