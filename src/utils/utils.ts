a224f4022664f5fece96a6ceff52786a98f1c3f4
Alex Kio <alexandermartelkio@gmail.com>
2021-07-26 22:00:27 -0400

    Map new objects from fetch Response

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,12 @@
-import React from "react";
-
-export {}
+export const cleanMarketsData = (response: {id: string, marketname: any}[]) => {
+  
+  let mapped = response.map(currentMarket => {
+    let name = currentMarket.marketname.split(' ')
+    let distance = name[0]
+    return {
+      id: currentMarket.id,
+      distanceFromZip: Math.round(distance * 10) / 10,
+      marketName: name.slice(1).join(' ')
+    }
+  })
+};
