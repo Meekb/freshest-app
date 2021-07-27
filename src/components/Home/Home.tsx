e933992fb7b281296364b781eb73af422124bfbe
Alex Kio <alexandermartelkio@gmail.com>
2021-07-27 14:06:19 -0400

    Refactor error check within Promise.all to utilize util function

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -44,13 +44,7 @@ const Home = ():JSX.Element => {
   const getDetails = (filteredMarkets: ApiMarkets["markets"]) => {
     Promise.all(filteredMarkets.map(currentMarket => {
       return fetchGetResponse(`mktDetail?id=${currentMarket.id}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        } else {
-          return response
-        }
-      })
+      .then(response => checkForError(response))
       .then(response => response.json())
     }))
     .then(arrayOfPromises => cleanDetailsData(arrayOfPromises))
