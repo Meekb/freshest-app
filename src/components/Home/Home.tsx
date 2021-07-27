0d2cfa05f21634be0bc8296b0cc4fca37dd80b7f
Alex Kio <alexandermartelkio@gmail.com>
2021-07-27 09:06:15 -0400

    Create promises array using Promise.all

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react'
+import { fetchGetResponse } from '../../utils/apiCalls'
+import { cleanMarketsData } from '../../utils/utils'
+
 
 interface ApiMarkets {
   markets: {
     id: number,
+    distanceFromZip: number,
     marketName: string
     //need logic to split the distance from the name to display the name
   }[],
@@ -18,18 +22,31 @@ interface ApiMarkets {
 const Home = ():JSX.Element => {
   const [ allMarkets, setMarkets ] = useState<ApiMarkets["markets"]>([])
   const [ marketDetails, setDetails ] = useState<ApiMarkets["marketDetails"]>([])
-  const [ error, setError ] = useState('')
-  
-  const getMarkets = async () => {
-    const url = 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=01070'
-    setError('')
+  const [ error, setError ] = useState(0)
 
+  const getMarkets = async () => {
     try {
-      const response = await fetch(url)
-      const markets = await response.json()
-      setMarkets(markets)
-    } catch(error) {
+      let response = await fetchGetResponse('zipSearch?zip=01701');
+      checkForError(response)
+      let data = await response.json();
+      let cleanedData = cleanMarketsData(data.results)
+      setMarkets(cleanedData)
+      getDetails(cleanedData)
+    } catch (error) {
       setError(error.message)
+    }
+  }
+
+  const getDetails = (filteredMarkets: ApiMarkets["markets"]) => {
+    let promises = Promise.all(filteredMarkets.map(currentMarket => {
+      return fetchGetResponse(`mktDetail?id=${currentMarket.id}`)
+    }))
+    
+  }
+
+  const checkForError = (response: Response) => {
+    if (!response.ok) {
+      // throw new Error(response);
     }
   }
 
