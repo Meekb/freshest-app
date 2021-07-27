ebbeb762bf2dee181b11b29a7581b9eeeec1707c
Alex Kio <alexandermartelkio@gmail.com>
2021-07-26 22:04:56 -0400

    Make id a number type

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,7 +4,7 @@ export const cleanMarketsData = (response: {id: string, marketname: any}[]) => {
     let name = currentMarket.marketname.split(' ')
     let distance = name[0]
     return {
-      id: currentMarket.id,
+      id: parseInt(currentMarket.id),
       distanceFromZip: Math.round(distance * 10) / 10,
       marketName: name.slice(1).join(' ')
     }
