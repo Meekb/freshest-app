9b8db162aa9984c7b15ad07571b14ff275d52d65
Alex Kio <alexandermartelkio@gmail.com>
2021-07-27 12:40:35 -0400

    Clean Details schedule data

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -17,11 +17,30 @@ export const cleanDetailsData = (arrayOfPromises: {}[]) => {
   return arrayOfPromises.map(currentMarket => {
     let addressArray = currentMarket.marketdetails.Address.split(',')
 
+    let schedule = currentMarket.marketdetails.Schedule.replaceAll('<br>', '').split(';')
+    schedule.pop()
+
+    let formattedSchedule = schedule.map(currentDay => {
+      let season
+      if (currentDay.split(': ')[0].slice(0,-4).length < 24) {
+        season = 'bad format'
+      } else {
+        season = currentDay.split(': ')[0].slice(0,-4)
+      }
+      
+      return {
+        dayOfWeek: currentDay.split(': ')[0].slice(-3),
+        time: currentDay.split(': ')[1],
+        season: season
+      }
+    })
+
     return {
       street: addressArray[0],
       city: addressArray[addressArray.length-3],
       state: addressArray[addressArray.length-2],
       zip: addressArray[addressArray.length-1],
+      schedule: formattedSchedule,
       products: currentMarket.marketdetails.Products.split(';'),
       mapsLink: currentMarket.marketdetails.GoogleLink
     }
