47b2188d78a8df2d05b068e0fdb836428cb94d7b
Beth <bethm.meeker@gmail.com>
2021-07-27 15:20:52 -0600

    Add Event type to event argument submitZip()

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,39 +1,46 @@
 import React, { useState } from 'react';
 import './Search.css';
 
-interface IProps_Click {
-  onclick: (event: React.MouseEvent<HTMLButtonElement>) => void;
-}
-
 const Search = () => {
 
-  const [zip, setZip] = useState('');
+  const [zip, setZip] = useState('')
 
-  const submitZipSearch = (event: <IProps_Click> ) => {
-    const newSearch = {
-      zip,
-    }
-    // call the Home method that retrieves the markets for that zip code
-    // findMarkets(newSearch);
-    clearInputs();
+  const submitZipSearch = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    console.log(zip);
+    clearInput();
   }
 
-  const clearInputs = () => {
+  const clearInput = () => {
     setZip('');
   }
 
-    // console.log(zip)
+    console.log(zip)
 
   return (
     <form>
-      <label>Enter your zip code: </label>
+      <label className='zip-label'>Enter your zip code: </label>
       <input 
-        type='number'
+        type='text' //can limit input with type text, but not number
         name='zip'
         placeholder='zip...'
-        onChange={e => setZip(e.target.value)}
+        // onChange={e => }
+        min='00001'
+        max='99999'
+        maxLength={5} // if using 'text' we will need to add logic to convert to a number and check validity
+        required
       />
-      <button className='find-btn' onClick={submitZipSearch}>Find Markets</button>
+      <label className='miles-label'>Find markets within:
+      <select name='miles' className='dropdown'>
+        <option value='10mi'>10mi</option>
+        <option value='25mi'>25mi</option>
+        <option value='50mi'>50mi</option>
+      </select>
+      </label>
+      <button 
+        className='find-btn' 
+        onClick={submitZipSearch}
+      >Find Markets</button>
     </form>
   );
 }
