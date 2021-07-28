f849de73e4c896493842d76d0296c186ef5f3270
Beth <bethm.meeker@gmail.com>
2021-07-28 11:40:09 -0600

    Start Details FC, write interface with markets and marketDetails

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React from "react";
+import "../Details/Details.css"
 
-interface DetailsProps {}
+interface MarketProps {
+  markets: {
+    id: number,
+    distanceFromZip: number,
+    marketName: string
+  }[],
+  marketDetails: {
+    street: string,
+    city: string,
+    state: string,
+    zip: string,
+    schedule: {
+      dayOfWeek: string,
+      time: string,
+      season: string
+    }[],
+    products: string[],
+    mapsLink: string
+  }[],
+}
 
-export const Details: React.FC<DetailsProps> = ({}) => {
-  return <></>;
-};
+const Details: React.FC<MarketProps> = ({ markets, marketDetails }) => {
+
+  return (
+    <section className='market-details'>
+      <h1>{markets.marketName}</h1>
+      <p>Address</p>
+      <p>Schedule</p>
+      <div className='products'>
+        <p>Products....Unordered List?</p>
+      </div>
+      <p>GoogleLink</p>
+    </section>
+  );
+
+}
+
+export default Details;
+
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react'
+import { fetchGetResponse } from '../../utils/apiCalls'
+import { cleanMarketsData, cleanDetailsData, checkForError } from '../../utils/utils'
+import Search from '../Search/Search'
+import Details from '../Details/Details'
+
+interface ApiMarkets {
+  markets: {
+    id: number,
+    distanceFromZip: number,
+    marketName: string
+  }[],
+  marketDetails: {
+    street: string,
+    city: string,
+    state: string,
+    zip: string,
+    schedule: {
+      dayOfWeek: string,
+      time: string,
+      season: string
+    }[],
+    products: string[],
+    mapsLink: string
+  }[]
+}
+
+const Home = ():JSX.Element => {
+  const [ allMarkets, setMarkets ] = useState<ApiMarkets["markets"]>([])
+  const [ marketDetails, setDetails ] = useState<ApiMarkets["marketDetails"]>([])
+  const [ error, setError ] = useState(0)
+
+   const getMarkets = async (zip: string) => {
+     console.log(zip)
+    try {
+      let response = await fetchGetResponse(`zipSearch?zip=${zip}`);
+      checkForError(response)
+      let data = await response.json();
+      let cleanedData = cleanMarketsData(data.results)
+      setMarkets(cleanedData)
+      getDetails(cleanedData)
+    } catch (error) {
+      setError(error)
+    }
+  }
+
+  const getDetails = (filteredMarkets: ApiMarkets["markets"]) => {
+    Promise.all(filteredMarkets.map(currentMarket => {
+      return fetchGetResponse(`mktDetail?id=${currentMarket.id}`)
+      .then(response => checkForError(response))
+      .then(response => response.json())
+    }))
+    .then(arrayOfPromises => cleanDetailsData(arrayOfPromises))
+    .then(cleanData => setDetails(cleanData))
+    .catch(error => setError(error))
+  }
+  
+  return (
+    <div className="home">
+      <h2>yellow</h2>
+      <Search getMarkets={getMarkets} />
+      {/* <Search allMarkets={allMarkets} marketDetails={marketDetails}/> */}
+      {/* <Results allMarkets={allMarkets} marketDetails={marketDetails}/> */}
+      <Details />
+    </div>
+  )
+}
+
+export default Home;
