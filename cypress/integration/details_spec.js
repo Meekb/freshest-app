e5924ac7ba7d426b440bef2c676964b8e989e91f
Alex Kio <alexandermartelkio@gmail.com>
2021-07-29 17:56:57 -0400

    Create test for back button

--- cypress/integration/details_spec.js
+++ cypress/integration/details_spec.js
@@ -2,38 +2,19 @@ describe('Details user flows', () => {
 
   beforeEach(() => {
     cy.loadList()
+      .get('article[id="1000006"]').click()
   });
 
   it('The url should be ', () => {
-
+    cy.url().should('include', '/1000006')
   });
 
-  it('Should have a header', () => {
-    cy.contains('Freshly Fetched')
+  it('It should display the name of the app', () => {
+    cy.get('h1').contains('Freshly Fetched')
   });
 
   it('The page should have a back button', () => {
-    
-  });
-
-  it('The page should display the market name', () => {
-    
-  });
-
-  it('The page should display the market schedule', () => {
-    
-  });
-
-  it('The page should display the market address', () => {
-    
-  });
-
-  it('The page should have a google maps link', () => {
-    
-  });
-
-  it('The page should display the market\'s products', () => {
-    
+    cy.get('button')
   });
 
 });
