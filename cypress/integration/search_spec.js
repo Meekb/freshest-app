3423846a6f80ae4a0f35df2e35589f768af1073d
Alex Kio <alexandermartelkio@gmail.com>
2021-07-29 17:50:43 -0400

    Create test for zip input value

--- cypress/integration/search_spec.js
+++ cypress/integration/search_spec.js
@@ -13,4 +13,11 @@ describe('Search user flows', () => {
     cy.contains('Enter your zip code to find markets near you')
   });
 
+  it('Should have a form that takes a 5 digit number between 1 and 99999', 
+  () => {
+    cy.get('input[name="zip"]')
+      .type('10101')
+      .should('have.value', '10101')
+  });
+
 });
