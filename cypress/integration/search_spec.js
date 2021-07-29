4d82ccf7c39242a3416ce20bfcb4ef19a88d8ccb
Alex Kio <alexandermartelkio@gmail.com>
2021-07-29 17:49:34 -0400

    Refactor test for header

--- cypress/integration/search_spec.js
+++ cypress/integration/search_spec.js
@@ -4,46 +4,9 @@ describe('Search user flows', () => {
     cy.visit('http://localhost:3000');
   });
 
-  it('When a user first visits the site it should have a header', () => {
-    cy.contains('Freshly Fetched')
-  });
-
-  it('When a user first visits the site it should contain instructions', () => {
-    cy.contains('Enter your zip code to find markets near you')
-  });
-
-  it('Should have a form that takes a 5 digit number between 1 and 99999', 
+  it('When a user first visits the site it should display the name of the app', 
   () => {
-    cy.get('input[name="zip"]')
-      .type('10101')
-      .should('have.value', '10101')
-  });
-
-  it('Should have a form that takes a 5 digit number between 1 and 99999', 
-  () => {
-    cy.get('input[name="zip"]')
-      .type('{enter}')
-  });
-
-  it('The zip input should not take a number less than 1', () => {
-    cy.get('input[name="zip"]')
-      .type('00000').type('{enter}')
-  });
-
-  it('The zip input should not take a number greater than 100000', () => {
-    cy.get('input[name="zip"]')
-      .type('100000').type('{enter}')
-  });
-
-  it('The submit button is disabled if the number isn\'t 5 digits in length',
-  () => {
-
-  });
-
-  it('The form input should only take numbers', () => {
-    cy.get('input[name="zip"]')
-      .type('abababa')
-      .should('have.value', '')
+    cy.get('h1').contains('Freshly Fetched')
   });
 
 });
