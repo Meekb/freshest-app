b749e55257609926456e23ba8e6d6d43a8b59c56
Alex Kio <alexandermartelkio@gmail.com>
2021-07-29 18:07:26 -0400

    Refactor form validity tests

--- cypress/integration/search_spec.js
+++ cypress/integration/search_spec.js
@@ -20,4 +20,50 @@ describe('Search user flows', () => {
       .should('have.value', '10101')
   });
 
+  it('The submit button is disabled if the number is less than 1', () => {
+    cy.get('input[name="zip"]')
+      .type('-12345')
+      .get('button')
+      .should('have.attr', 'disabled').and('eq', 'true')
+  });
+
+  it('The submit button is disabled if the number is greater than 100000', 
+  () => {
+    cy.get('input[name="zip"]')
+    .type('100000')
+    .get('button')
+    .should('have.attr', 'disabled').and('eq', 'true')
+  });
+
+  it('The submit button is disabled if the number isn\'t a whole number', 
+  () => {
+    cy.get('input[name="zip"]')
+    .type('100.00')
+    .get('button')
+    .should('have.attr', 'disabled').and('eq', 'true')
+  });
+
+  it('The submit button is disabled if the number isn\'t 5 digits in length',
+  () => {
+    cy.get('input[name="zip"]')
+      .type('1000')
+      .get('button')
+      .should('have.attr', 'disabled').and('eq', 'true')
+  });
+
+  it('The form input should only take numbers', () => {
+    cy.get('input[name="zip"]')
+      .type('abababa')
+      .should('have.value', '')
+  });
+
+  it('The form contains a drop down for a mileage filter that has a default value', 
+  () => {
+
+  });
+
+  it('The submit button routes the user to the List component', () => {
+
+  });
+
 });
