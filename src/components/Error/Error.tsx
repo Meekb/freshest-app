0c4f047e85308a67d9d0c1db2814489e3d9ab844
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-07-30 16:51:39 -0600

    Add error component structure and basic conditional rendering

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
-interface ErrorProps {}
+interface ErrorProps {
+    server: string;
+    client: string;
+    loading: string;
+}
 
 export const Error: React.FC<ErrorProps> = ({}) => {
-  return <></>;
+  const [ serverError, setServerError ] = useState<ErrorProps["server"]>("");
+  const [ clientError, setClientError ] = useState<ErrorProps["client"]>("")
+  const [ loading, setLoading ] = useState<ErrorProps["loading"]>("")
+
+
+  return (
+    <>
+    { serverError && <h2>Server Error</h2> }
+    { clientError && <h2>Sorry, page not found. please try again</h2> }
+    { loading && <h2>Loading...</h2> }
+    </>
+  );
 };
