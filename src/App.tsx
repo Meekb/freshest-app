440f3a666338f1a8cfc6b86ec90e36487cd7b49f
Alex Kio <alexandermartelkio@gmail.com>
2021-07-30 16:47:59 -0400

    Add schedule to market and id to details

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { getData } from './utils/apiCalls';
 import {
   cleanMarketsData,
   cleanDetailsData,
+  addScheduleToMarkets,
   checkForError
 } from './utils/utils';
 import { Results } from './components/Results/Results';
@@ -18,8 +19,10 @@ interface ApiMarkets {
     id: number;
     distanceFromZip: number;
     marketName: string;
+    schedule: {};
   }[];
   marketDetails: {
+    id: number;
     street: string;
     city: string;
     state: string;
@@ -49,7 +52,6 @@ export const App: React.FC = () => {
       checkForError(response);
       let data = await response.json();
       let cleanedData = cleanMarketsData(data.results, distance);
-      setMarkets(cleanedData);
       getDetails(cleanedData);
       history.push('/markets');
     } catch (error) {
@@ -62,13 +64,18 @@ export const App: React.FC = () => {
       filteredMarkets.map(currentMarket => {
         return getData(`mktDetail?id=${currentMarket.id}`)
           .then(response => checkForError(response))
-          .then(response => response.json());
+          .then(response => response.json())
+          .then(data => cleanDetailsData(data.marketdetails, currentMarket.id))
       })
     )
-      .then(arrayOfPromises => cleanDetailsData(arrayOfPromises))
-      .then(cleanData => setDetails(cleanData))
-      .catch(error => setError(error));
+    .then(data => addScheduleToMarkets(data, filteredMarkets))
+    .then(completeData => setData(completeData))
   };
+
+  const setData = (data: any) => {
+    setMarkets(data.markets)
+    setDetails(data.marketDetails)
+  }
 
   return (
     <>
